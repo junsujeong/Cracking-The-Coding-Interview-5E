4eae596c7a3dd1b8464b70dbf8ef2de99fd50321
junsu jeong <junsu852@naver.com>
2017-09-18 00:14:32 -0500

    Initial studying

---
+++ 1.C++/lastLine.cpp
@@ -0,0 +1,21 @@
+
+int main()
+{
+
+}
+
+void printLastLine()
+{
+   const int LAST= 5;
+   ifstream file ( fileName);
+	string L[k];
+   int size = 0;
+
+   while (file.good()){
+      getLine(file, L[size % LAST];
+      size++;
+       }
+
+
+
+}
